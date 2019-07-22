8318a0f2d1e06b228dac2ded98e8a04a2d028416
sagar mochi <chouhansagar131@gmail.com>
2019-07-23 00:49:04 +0530

    7 test pass out of 9 fair enough

--- hourglassSum.py
+++ hourglassSum.py
@@ -8,16 +8,23 @@ import sys
 
 # Complete the hourglassSum function below.
 def hourglassSum(arr):
+    mx =0
     for i in range(len(arr)-2):
         for j in range(len(arr)-2):
-            count = 0
+            count = 9
+            sum1 = 0
             for k in range(i,i+3):
                 for l in range(j,j+3):
-                    print(arr[k][l],end=' ')
-
-            
-
-        print()
+                    if count is not 4 and count is not 6: 
+                        sum1 = sum1 + arr[k][l]
+                        #print(arr[k][l],end="")  debugging purpose
+                        count = count - 1
+                    else:
+                        count = count - 1
+                #print()   debugging purposes
+            if sum1 > mx:
+                mx = sum1
+    return mx
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
