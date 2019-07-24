734c3b7bccd94b1bba70aa6704ce74cc892c30b1
sagar mochi <chouhansagar131@gmail.com>
2019-07-25 01:37:39 +0530

    optimized solution

---
+++ missingNum.py
@@ -0,0 +1,18 @@
+t = int(input())
+while t is not 0:
+    t = t - 1
+    n = int(input())
+    lst = set([int(num) for num in input().split()])
+    lst2 = set(list(range(1,n+1)))
+    #print(lst)
+    k=str(lst2.difference(lst)).replace('{','').replace('}','')
+    print(k)
+    
+    #for i in lst2:
+        #if i not in lst:
+            #print(i)
+            #break
+    
+        
+/* for non optimized solution create a list with full number and
+         match with our main list    */
