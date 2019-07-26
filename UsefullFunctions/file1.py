3928e8a51d70bca27772a1adcd960d6f8eee7831
sagar mochi <chouhansagar131@gmail.com>
2019-07-26 23:00:01 +0530

    file1 created

---
+++ UsefullFunctions/file1.py
@@ -0,0 +1,7 @@
+from operator import * 
+arr = [ 1, 2, 3, 3, 3 ] 
+arr1 = [ 'a', 'b', 'c', 'b', 'd' ] 
+  
+print("Number of occurence of b in arr1 =", countOf(arr1, "b")) 
+print("Number of occurence of e in arr1 =", countOf(arr1, "e")) 
+print("Number of occurence of 3 in arr =", countOf(arr, 3)) 
