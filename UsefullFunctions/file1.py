cc212ac28b536ce176df8bdc6f3a4e0414faed20
sagar mochi <chouhansagar131@gmail.com>
2019-08-06 02:08:26 +0530

    updated file1.py new function

--- UsefullFunctions/file1.py
+++ UsefullFunctions/file1.py
@@ -5,3 +5,9 @@ arr1 = [ 'a', 'b', 'c', 'b', 'd' ]
 print("Number of occurence of b in arr1 =", countOf(arr1, "b")) 
 print("Number of occurence of e in arr1 =", countOf(arr1, "e")) 
 print("Number of occurence of 3 in arr =", countOf(arr, 3)) 
+
+
+#######################################################################################
+from math import fabs
+
+math.fabs(number)    # will return the absolute value(positive value) of single interger
