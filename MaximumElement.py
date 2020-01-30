4196794511779bdb337e0ac3742af8698adc4873
sagar mochi <chouhansagar131@gmail.com>
2020-01-30 23:38:57 +0530

    optimized solution

--- MaximumElement.py
+++ MaximumElement.py
@@ -7,18 +7,17 @@ for _ in range(n):
     lst = list(map(int,input().split(" ")))
     if lst[0] == 1:
         stack.append(lst[1])
-        if lst[1] > max_num:
+        if lst[1] >= max_num:
             max_num = lst[1]
             max_stack.append(max_num)
     if lst[0] == 2:
-        if stack:
-            value=stack.pop()
-        if max_stack[len(max_stack)-1] == value:
+        value=stack.pop()
+        if max_stack[-1] == value:
             max_stack.pop()
-            max_num = max_stack[len(max_stack)-1]
+            max_num = max_stack[-1]
                         
     if lst[0] == 3:
-        print(max_stack[len(max_stack)-1])
+        print(max_stack[-1])
 
 
 
